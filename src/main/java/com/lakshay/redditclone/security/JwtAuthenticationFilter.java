46f98effd1dd17b3bbd99bd16eec0dbbd931ae33
Lakshay <Lakshay@DESKTOP-AJ7C0RG>
2020-07-20 22:49:35 +0530

    error resolved ready to merge

--- src/main/java/com/lakshay/redditclone/security/JwtAuthenticationFilter.java
+++ src/main/java/com/lakshay/redditclone/security/JwtAuthenticationFilter.java
@@ -31,7 +31,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 		String jwt=getJwtFromRequest(request);
-		jwtProvider.validateToken(jwt);
+		
 		//getting username
 		 if (StringUtils.hasText(jwt) && jwtProvider.validateToken(jwt)) {
 	            String username = jwtProvider.getUsernameFromJwt(jwt); //getting uname
